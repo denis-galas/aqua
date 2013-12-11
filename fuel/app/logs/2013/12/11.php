c0e365830bcb6cff55f5445dad4b864d0463ede8
Denis Qarea <denis.qarea@gmail.com>
2013-12-11 09:39:54 +0200

    fuelphp initial

---
+++ fuel/app/logs/2013/12/11.php
@@ -0,0 +1,6 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+WARNING - 2013-12-11 09:30:52 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2013-12-11 09:34:47 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2013-12-11 09:35:48 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2013-12-11 09:35:54 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
