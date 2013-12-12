13a7e05819c209db005286b32d907cf322293b9b
Denis Qarea <denis.qarea@gmail.com>
2013-12-12 11:34:37 +0200

    footer fix-bottom, styles changes

--- fuel/app/views/template.php
+++ fuel/app/views/template.php
@@ -6,44 +6,51 @@
 	<?php echo Asset::css('bootstrap.min.css'); ?>
 	<?php echo Asset::css('bootstrap-theme.min.css'); ?>
 	<?php echo Asset::css('styles.css'); ?>
+	
 	<?php echo Asset::js('jquery-1.10.2.min.js'); ?>
 	<?php echo Asset::js('bootstrap.min.js'); ?>
 </head>
 <body>
-	<div class="container">
-		<nav role="navigation" class="navbar navbar-inverse">
-			<!-- Brand and toggle get grouped for better mobile display -->
-			<div class="navbar-header">
-				<button data-target="#bs-example-navbar-collapse-9" data-toggle="collapse" class="navbar-toggle" type="button">
-					<span class="sr-only">Toggle navigation</span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</button>
-				<a href="/" class="navbar-brand">Brand</a>
+	<div id="wrap">
+		<div class="container">
+			<nav role="navigation" class="navbar navbar-inverse">
+				<!-- Brand and toggle get grouped for better mobile display -->
+				<div class="navbar-header">
+					<button data-target="#bs-example-navbar-collapse-9" data-toggle="collapse" class="navbar-toggle" type="button">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a href="/" class="navbar-brand">Brand</a>
+				</div>
+	
+				<?php $active_url = \Request::active()->route->translation;?>
+				<!-- Collect the nav links, forms, and other content for toggling -->
+				<div id="bs-example-navbar-collapse-9" class="collapse navbar-collapse">
+					<ul class="nav navbar-nav">
+						<li <?php if(stripos($active_url,'index') !== false) echo "class='active'"?>><a href="/">Главная</a></li>
+						<li <?php if(stripos($active_url,'prices') !== false) echo "class='active'"?>><a href="/prices">Прайсы</a></li>
+						<li <?php if(stripos($active_url,'contacts') !== false) echo "class='active'"?>><a href="/contacts">Контакты</a></li>
+					</ul>
+				</div><!-- /.navbar-collapse -->
+			</nav>
+			
+			<div class="raw content">
+				<?php echo $content; ?>
 			</div>
-
-			<?php $active_url = \Request::active()->route->translation;?>
-			<!-- Collect the nav links, forms, and other content for toggling -->
-			<div id="bs-example-navbar-collapse-9" class="collapse navbar-collapse">
-				<ul class="nav navbar-nav">
-					<li <?php if(stripos($active_url,'index') !== false) echo "class='active'"?>><a href="/">Главная</a></li>
-					<li <?php if(stripos($active_url,'prices') !== false) echo "class='active'"?>><a href="/prices">Прайсы</a></li>
-					<li <?php if(stripos($active_url,'contacts') !== false) echo "class='active'"?>><a href="/contacts">Контакты</a></li>
-				</ul>
-			</div><!-- /.navbar-collapse -->
-		</nav>
-		
-		<div class="raw content">
-			<?php echo $content; ?>
 		</div>
+	</div>
 		
-		<div class="footer">
-			<div class="pull-right">Copyright (c) 2013</div>
-			<address>
-				Тел. (123) 456-7890<br>
-				Тел. (123) 456-7890
-			</address>
+	<div id="footer">
+		<div class="container">
+			<div class="foot-inside">
+				<div class="pull-right">Copyright (c) 2013</div>
+				<address>
+					Тел. (123) 456-7890<br>
+					Тел. (123) 456-7890
+				</address>
+			</div>
 		</div>
 	</div>
 	
@@ -66,6 +73,12 @@
 							<label for="inputPassword3" class="col-sm-2 control-label">Сообщение</label>
 							<div class="col-sm-10">
 								<textarea class="form-control" rows="5"></textarea>
+							</div>
+						</div>
+						<div class="form-group">
+							<label for="inputPassword3" class="col-sm-2 control-label">Файл</label>
+							<div class="col-sm-10">
+								<input type="file"/>
 							</div>
 						</div>
 						<div class="form-group">
