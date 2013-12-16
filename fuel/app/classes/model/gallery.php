845d70dec1edb46b00ee7b7b3855220aa6786033
Denis Qarea <denis.qarea@gmail.com>
2013-12-16 16:30:02 +0200

    gallery model

---
+++ fuel/app/classes/model/gallery.php
@@ -0,0 +1,34 @@
+<?php
+
+class Model_Gallery extends Orm\Model
+{
+	protected static $_table_name = 'gallery';
+	protected static $_primary_key = array('id');
+	protected static $_properties = array (
+			'id', 'title', 
+			'category' => array(
+				'data_type' => 'int',
+			)
+			, 'source'
+			);
+
+	/**
+	 * this function retrieve user object by given field
+	 */
+	public static function getRecord($field, $value)
+	{
+		$query = self::query()->where($field, $value);
+	
+		return $query->get_one();
+	}
+	
+	/**
+	 * this function check if same record with already exists in entered field
+	 */
+	public static function isRecordExists($field, $value)
+	{
+		$query = self::query()->where($field, $value);
+	
+		return (bool) $query->count();
+	}
+}
---
+++ fuel/app/migrations/004_gallery_create.php
@@ -0,0 +1,29 @@
+<?php
+namespace Fuel\Migrations;
+
+class Gallery_create
+{
+
+	function up()
+	{
+		if(!\DBUtil::table_exists('gallery')) {
+			\DBUtil::create_table(
+					'gallery',
+					array(
+							'id' => array('constraint' => 10, 'type' => 'int', 'auto_increment' => true, 'unsigned' => true),
+							'title' => array('constraint' => 255, 'type' => 'varchar'),
+							'category' => array('type' => 'int'),
+							'source' => array('constraint' => 255, 'type' => 'varchar'),
+					),
+					array('id'), false, 'InnoDB', 'utf8_general_ci'
+			);
+		}
+	}
+
+	function down()
+	{
+		if(\DBUtil::table_exists('gallery')) {
+			\DBUtil::drop_table('gallery');
+		}
+	}
+}
